e9a0188b0b6c58f0b88cde35e626448a24a8dcc4
Dylan Pople <drpople14@gmail.com>
2017-10-04 16:29:28 -0600

    test
    Summary:
    Left Menu component, accordion list, right sticky
    
    created components for left menu, accordion list and right sticky into one component named ModuleMenu
    
    start courses controller
    
    basic footer designed, needs better styling
    
    prepare for merge with master
    
    intitial dev commit
    
    Left Menu component, accordion list, right sticky
    
    created components for left menu, accordion list and right sticky into one component named ModuleMenu
    
    start courses controller
    
    basic footer designed, needs better styling
    
    initial commit
    
    resolving merge
    
    testing
    
    Left Menu component, accordion list, right sticky
    
    created components for left menu, accordion list and right sticky into one component named ModuleMenu
    
    start courses controller
    
    basic footer designed, needs better styling
    
    prepare for merge with master
    
    intitial dev commit
    
    Left Menu component, accordion list, right sticky
    
    created components for left menu, accordion list and right sticky into one component named ModuleMenu
    
    start courses controller
    
    basic footer designed, needs better styling
    
    initial commit
    
    resolving merge
    
    implemented left menu, accordion, and right sticky tab into home.js
    
    implemented group, section, and right-tab components
    
    test
    
    testing
    
    basic footer designed, needs better styling
    
    added section and group models
    
    Reviewers: #staff, spencer.richards, derekdapp
    
    Reviewed By: derekdapp
    
    Differential Revision: http://phabricator.devpointstudios.com/D102

--- db/schema.rb
+++ db/schema.rb
@@ -73,10 +73,6 @@ ActiveRecord::Schema.define(version: 20171003223457) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.text "bio"
-    t.boolean "is_admin"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
