df9f0566f555b26d504da846ceb2235518572c86
Robert Scarlett <scarlettt10@msn.com>
2017-10-10 14:34:30 -0600

    add seed file
    Reviewers: #staff, spencer.richards
    
    Reviewed By: #staff, spencer.richards
    
    Differential Revision: http://phabricator.devpointstudios.com/D121

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do 
+  @course = Course.create(
+    course_type: Faker::Zelda.game,
+    term: Faker::Zelda.location,
+    year: Faker::Number.number(4)
+  )
+   
+    10.times do    
+      section = Section.create(
+        title: Faker::Company.catch_phrase,
+        course_id: @course.id
+      )
+
+      10.times do 
+        group = Group.create(
+          title: Faker::Company.catch_phrase,
+          section_id: section.id
+        )
+      end
+    end
+end
+
+admin = User.create(
+  first_name: 'admin',
+  last_name: 'admin',
+  email: 'admin@admin.com',
+  password: 'password',
+  is_admin: true
+  )
+
+  1.times do 
+    student = User.create(
+      first_name: 'student',
+      last_name: 'student',
+      email: 'test@test.com',
+      password: 'password',
+      is_admin: false
+      )
+
+        Enrollment.create(
+          role: 'student',
+          sub_role: Faker::Company.bs,
+          user_id: student.id,
+          course_id: @course.id
+        )
+  end
+
+puts "Success"
