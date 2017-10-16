0df785e258b3444b80b2dc68e9d54ab4d6662be4
Treven Trujillo <treventrujillo@gmail.com>
2017-10-16 14:32:35 -0600

    fixed footer error
    Summary: fixed footer error
    
    Reviewers: #staff, #portal, spencer.richards
    
    Reviewed By: #staff, #portal, spencer.richards
    
    Differential Revision: http://phabricator.devpointstudios.com/D249

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,29 +1,68 @@
 import React from 'react';
-import { Segment, Grid, Image } from 'semantic-ui-react';
+import {
+  Segment,
+  Grid,
+  Image,
+  Menu,
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
-  <Segment basic>
-    <Segment textAlign='center' basic style={styles.footer}>
-      <Image
-        src='https://media.licdn.com/media/AAEAAQAAAAAAAAfcAAAAJDI5MjNjMmJjLTg5NjktNGUzNy04ZDAwLTNhODI5NmFhZjM4YQ.png'
-        size='medium'
-        centered
-      />
-      {/* TODO: some image here */}
-      <hr />
-      {/* TODO: maybe some links here? */}
-      <Grid>
-        <Grid.Column textAlign='center' width={4}>
-          <a href='http://www.devpointlabs.com/'>DevPoint Labs</a>
+  <Segment fluid style={styles.container}>
+    <Segment textAlign='center' basic >
+      <Grid columns={3}>
+        <Grid.Column>
+          <a href='http://www.devpointlabs.com/'>
+            <Image
+              src='https://media.licdn.com/media/AAEAAQAAAAAAAAfcAAAAJDI5MjNjMmJjLTg5NjktNGUzNy04ZDAwLTNhODI5NmFhZjM4YQ.png'
+              size='medium'
+              style={styles.icons}
+            />
+          </a>
         </Grid.Column>
-        <Grid.Column textAlign='center' width={4}>
-          <a href='http://phabricator.devpointstudios.com/project/board/2/'>Phabricator Board</a>
+        <Grid.Column>
+          <a href='http://www.stackoverflow.com'>
+            <Image
+              src='http://www.computerscience.org/wp-content/uploads/2015/10/Stackoverflow.png'
+              size='medium'
+              style={styles.icons}
+            />
+          </a>
         </Grid.Column>
-        <Grid.Column textAlign='center' width={4}>
-          <a href='https://stackoverflow.com/'>Stack Overflow</a>
+        <Grid.Column>
+          <a href='http://www.github.com'>
+            <Image
+              src='https://4.bp.blogspot.com/-r4FaHOFh23U/WQYT2ynYqLI/AAAAAAAAEbg/BacAEeR-p_EyjyWRhsT5UK3mEWxVPhOiQCLcB/s400/github.7433692cabbfa132f34adb034e7909fa.jpg'
+              size='medium'
+              style={styles.icons}
+            />
+          </a>
         </Grid.Column>
-        <Grid.Column textAlign='center' width={4}>
-          <a href='http://i.imgur.com/TWTnn.jpg'>Me</a>
+      </Grid>
+    </Segment>
+    <Segment inverted color='grey' attached='top'>
+      <Grid columns={2}>
+        <Grid.Column>
+          <p style={styles.text}>
+            <i>Designed by DevPoint Studios</i>
+          </p>
+        </Grid.Column>
+        <Grid.Column textAlign='right'>
+          <Link to='/' style={styles.leftNav}>
+            <p>
+              Home /
+          </p>
+          </Link>
+          <Link to='/users' style={styles.leftNav}>
+            <p>
+              Users /
+          </p>
+          </Link>
+          <Link to='/' style={styles.leftNav}>
+            <p>
+              Support
+          </p>
+          </Link>
         </Grid.Column>
       </Grid>
     </Segment>
@@ -35,7 +74,19 @@ const styles = {
     backgroundColor: '#FFF',
     position: 'relative'
   },
-
+  container: {
+    padding: '0',
+  },
+  text: {
+    color: 'white',
+  },
+  leftNav: {
+    display: 'inline-block',
+    color: 'white',
+  },
+  icons: {
+    display: 'inline-block',
+  },
 }
 
 export default Footer;
