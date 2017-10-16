0c77e3d7ff31dcbdc841513f04969c9980d00aef
Treven Trujillo <treventrujillo@gmail.com>
2017-10-16 15:22:45 -0600

    heroheader moved from courseview to home component
    Reviewers: #staff, #portal, spencer.richards
    
    Reviewed By: #staff, #portal, spencer.richards
    
    Differential Revision: http://phabricator.devpointstudios.com/D254

--- client/src/components/CourseView.js
+++ client/src/components/CourseView.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { Container, Grid, Header, Segment, Sticky } from 'semantic-ui-react';
-import ImgHero from '../assets/images/hero-image.png';
 import SectionSelect from './SectionSelect';
 import SectionShow from './SectionShow';
 import RightTab from './RightTab';
 import NavBarSecondary from './NavBarSecondary';
-import {
-  HeroHeader,
-  HeroHeaderTextContainer,
-} from '../styles/styles';
+
 
 class CourseView extends Component {
   state = {
@@ -28,21 +24,8 @@ class CourseView extends Component {
 
     return (
       <div ref={this.handleContextRef}>
-        {/* ---------- header --------- */}
-        <Container fluid>
-          <HeroHeader bgImage={ImgHero}>
-            <div className='layer'>
-              <HeroHeaderTextContainer>
-                <Header as='h1' style={styles.h1}>{'Portal'}</Header>
-                <Header as='h3' style={styles.h3}>{'by DevPoint'}</Header>
-                <br/>
-              </HeroHeaderTextContainer>
-            </div>
-          </HeroHeader>
-          <NavBarSecondary />
-          <Segment basic />
-        </Container>
-
+        <NavBarSecondary />
+        <Segment basic />
         {/* ---------- 3 col grid --------- */}
         <Container style={styles.noBorder}>
           <Grid style={styles.noBorder}>
@@ -76,16 +59,6 @@ const styles = {
   },
   fluidHeader: {
     minHeight:'300px',
-  },
-  h1: {
-    color:'#FFF',
-    fontSize: '5em',
-  },
-  h3: {
-    color:'#FFF',
-    fontSize: '2.5em',
-    paddingTop: '0',
-    marginTop: '-25px',
   },
   noBorder: {
     border:'none',
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import ImgHero from '../assets/images/hero-image.png';
+import { Container, Header, Segment, } from 'semantic-ui-react';
+import {
+  HeroHeader,
+  HeroHeaderTextContainer,
+} from '../styles/styles';
 
 class Home extends Component {
   render () {
     return(
-      <h1>Home Component</h1>
+      <Container fluid>
+        <HeroHeader bgImage={ImgHero}>
+          <div className='layer'>
+            <HeroHeaderTextContainer>
+              <Header as='h1' style={styles.h1}>{'Portal'}</Header>
+              <Header as='h3' style={styles.h3}>{'by DevPoint'}</Header>
+              <br />
+            </HeroHeaderTextContainer>
+          </div>
+        </HeroHeader>
+      </Container>
     )
   }
+}
+
+const styles = {
+  h1: {
+    color: '#FFF',
+    fontSize: '5em',
+  },
+  h3: {
+    color: '#FFF',
+    fontSize: '2.5em',
+    paddingTop: '0',
+    marginTop: '-25px',
+  },
 }
 
 export default Home;
