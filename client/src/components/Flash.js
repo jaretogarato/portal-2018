e6291f14fd98c2115b69c43754df91c2d90c3e64
derekdapp <derekdapp@gmail.com>
2017-10-24 22:27:58 -0600

    Floating Message
    Reviewers: #staff, #portal, taylorwu21
    
    Reviewed By: #staff, #portal, taylorwu21
    
    Differential Revision: http://phabricator.devpointstudios.com/D359

--- client/src/components/Flash.js
+++ client/src/components/Flash.js
@@ -2,25 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Message, Container, Header } from 'semantic-ui-react';
 import { clearFlash } from '../actions/flash';
+import { FloatingMessage } from '../styles/flashStyles';
 
 const fadeFlash = dispatch => {
   setTimeout(() => {
     dispatch(clearFlash());
-  }, 15000);
+  }, 5000);
 };
 
 const Flash = ({ flash, dispatch }) => {
   if (flash.message) {
     return (
-      <Container>
-        <Message
-          onDismiss={() => dispatch(clearFlash())}
-          color={flash.color}
-        >
-          <Header as='h5' textAlign='center'>{flash.message}</Header>
-          {fadeFlash(dispatch)}
-        </Message>
-      </Container>
+      <FloatingMessage>
+        <Container>
+          <Message
+            onDismiss={() => dispatch(clearFlash())}
+            color={flash.color}
+          >
+            <Header as='h5' textAlign='center'>{flash.message}</Header>
+            {fadeFlash(dispatch)}
+          </Message>
+        </Container>
+      </FloatingMessage>
     );
   }
   return null;
---
+++ client/src/styles/flashStyles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const FloatingMessage = styled.div`
+  position: absolute;
+  z-index: 100;
+  top: 100px;
+  left: 0px;
+  right: 0px;
+`
