907429306bb4132740ed1839ed08953849a59e6c
FuChai Mead <jessie.mf.wolff@gmail.com>
2017-12-21 15:40:36 -0700

    quiz view page, quiz form made
    Summary: quiz form updated
    
    Reviewers: #staff, #portal, bryan-leano
    
    Reviewed By: #portal, bryan-leano
    
    Differential Revision: http://phabricator.devpointstudios.com/D656

---
+++ client/src/components/Rift/quizzes/QuizForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class QuizForm extends Component {
+
+  render(){
+    return(
+      <div>
+        Quiz Form to create a quiz 
+      </div> 
+    )
+  }
+}
+
+export default QuizForm;
+
+
--- client/src/components/Rift/quizzes/Quizzes.js
+++ client/src/components/Rift/quizzes/Quizzes.js
@@ -1,19 +1,55 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { Header, Table, Container, Button, Icon, Grid } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class Quizzes extends Component {
 
-
-
-
-
   render() {
     return (
-      <div>
-        This is the Quiz Component</div>
+      <Container> 
+       <Header textAlign='center' style={styles.quiz} > Quizzes  </Header> 
+        <Grid>
+          <Grid.Row> 
+            <Grid.Column width={14}>
+            </Grid.Column>
+            <Grid.Column width={2}>
+             <Button icon labelPosition='left'>
+              <Icon name='add' />
+              Quiz
+              </Button> 
+            </Grid.Column>
+          </Grid.Row> 
+          <Grid.Row>
+            <Grid.Column> 
+              <Table singleLine>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell width={6}>Name</Table.HeaderCell>
+                <Table.HeaderCell width={4}> Created At</Table.HeaderCell>
+                <Table.HeaderCell width={4}>Created By</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+            <Table.Row>
+              <Table.Cell>First Quiz</Table.Cell>
+              <Table.Cell>December 21, 2017</Table.Cell>
+              <Table.Cell>FuChai Mead</Table.Cell>
+            </Table.Row>
+            </Table.Body>
+          </Table> 
+            </Grid.Column>
+          </Grid.Row>
+        </Grid> 
+      </Container> 
     )
   }
 
+}
+
+const styles = {
+  quiz: {
+    paddingTop: '2%',
+  }
 }
 
 export default Quizzes;
