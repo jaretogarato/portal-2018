1716147119fb339e9765c2ebe290794fc2d5bbf4
Dan Actor <danltactor@gmail.com>
2017-12-21 14:54:38 -0700

    add grades link to courseSideNav
    Reviewers: #portal, anthonymkelley
    
    Reviewed By: #portal, anthonymkelley
    
    Differential Revision: http://phabricator.devpointstudios.com/D652

--- client/src/components/CourseSideNav.js
+++ client/src/components/CourseSideNav.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 class CourseSideNav extends React.Component {
 
   render() {
-    const links = ['people', 'attendance'];
+    const links = ['people', 'attendance', 'grades'];
     const { match: { params: { id }}} = this.props;
     return links.map( (link, i) => 
-      <Link key={i} to={`/courses/${id}/${link}`}>{link}</Link>
+      <Link key={i} to={`/courses/${id}/${link}`}> {link} </Link>
     )
   }
 }
