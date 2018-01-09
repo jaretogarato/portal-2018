d227ca904386284651e32aab610237d2b08ece07
Zach Byers <byerszach@gmail.com>
2018-01-09 13:34:02 -0700

    dynamically set current course in attendance, add loader
    Reviewers: #staff, djungst
    
    Reviewed By: #staff, djungst
    
    Subscribers: djungst
    
    Differential Revision: http://phabricator.devpointstudios.com/D810

--- client/src/components/attendance/DatePicker.js
+++ client/src/components/attendance/DatePicker.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import moment from 'moment';
-import { Button, Container, Grid, Header } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Loader, Dimmer } from 'semantic-ui-react';
 import { getDate, updateDate } from '../../actions/currentDate';
 import { connect } from 'react-redux';
 
 class DatePicker extends React.Component {
+  state = { loaded: false }
 
   componentDidMount() {
     const { dispatch } = this.props;
@@ -19,46 +20,76 @@ class DatePicker extends React.Component {
     dispatch(updateDate(newDate));
   }
 
+  componentDidMount() {
+    this.hasCourse()
+  }
+
+  componentDidUpdate() {
+    this.hasCourse()
+  }
+
+  hasCourse = () => {
+    if (!this.state.loaded && this.props.course.id)
+      this.setState({ loaded: true })
+  }
+
+  isLoaded = () => {
+    const { currentDate, course } = this.props;
+    if (this.state.loaded){
+      return(
+        <Grid stackable columns='equal'>
+          <Grid.Row stretched>
+              <Grid.Column>
+                <Header as='h1'>
+                {`${course.course_type} ${course.term} ${course.year}`}
+                </Header>
+              </Grid.Column>
+              <Grid.Column>
+                <Header as='h1' textAlign="right">
+                  { currentDate }
+                </Header>
+                  <Container textAlign='right'>
+                    <Button.Group>
+                      <Button
+                        content='Prev'
+                        icon='angle double left'
+                        labelPosition='left'
+                        onClick={() => this.handleDayChange(-1)}
+                      />
+                      <Button
+                        content='Next'
+                        icon='angle right double'
+                        labelPosition='right'
+                        onClick={() => this.handleDayChange(1)}
+                      />
+                    </Button.Group>
+                  </Container>
+              </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      )
+    } else {
+      return (
+        <Dimmer active>
+          <Loader>Loading... </Loader>
+        </Dimmer>
+      )
+    }
+  }
+
   render() {
-    const { currentDate } = this.props;
-    return(
-      <Grid stackable columns='equal'>
-        <Grid.Row stretched>
-            <Grid.Column>
-              <Header as='h1'>
-                Full-Time Summer 2017
-                {/*TODO: Get current course name somehow */}
-              </Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header as='h1' textAlign="right">
-                { currentDate }
-              </Header>
-                <Container textAlign='right'>
-                  <Button.Group>
-                    <Button
-                      content='Prev'
-                      icon='angle double left'
-                      labelPosition='left'
-                      onClick={() => this.handleDayChange(-1)}
-                    />
-                    <Button
-                      content='Next'
-                      icon='angle right double'
-                      labelPosition='right'
-                      onClick={() => this.handleDayChange(1)}
-                    />
-                  </Button.Group>
-                </Container>
-            </Grid.Column>
-        </Grid.Row>
-      </Grid>
+    return (
+      <div>
+        {
+          this.isLoaded()
+        }
+      </div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
-  return { currentDate: state.currentDate }
+  return { currentDate: state.currentDate, course: state.course }
 }
 
 export default connect(mapStateToProps)(DatePicker);
