fcb957a100cb6fd662d2162aec8ef71aab6ff20a
Spencer Richards <spencer.richards7@gmail.com>
2018-01-09 11:01:02 -0700

    basic format for mobile
    Summary:
    add sidenav icon with mobile response
    
    add sidebar with pusher - styling needed
    
    move routes to function and add push animation
    
    add styles to grid and sidebar
    
    Reviewers: #staff, #portal, djungst
    
    Reviewed By: #staff, #portal, djungst
    
    Differential Revision: http://phabricator.devpointstudios.com/D833

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { setActiveCourse } from '../actions/course';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setActiveCourse } from '../actions/course';
 import { Route, Switch } from 'react-router-dom';
-import { Container, Grid, Segment } from 'semantic-ui-react';
-import CourseSideNav from './tether/CourseSideNav';
-import People from './tether/People';
-import SectionSelect from './tether/SectionSelect'
+import { Container, Grid, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react';
+import CourseSideNav from './course/CourseSideNav';
+import People from './people/People';
+import SectionSelect from './course/SectionSelect'
 import CourseHome from './course/CourseHome';
-import CourseSettings from './tether/CourseSettings';
-import PeopleProfile from './tether/PeopleProfile';
+import CourseSettings from './course/CourseSettings';
+import PeopleProfile from './people/PeopleProfile';
 import Assignment from './assignments/Assignment';
 import Quizzes from './quizzes/Quizzes';
 import QuizForm from './quizzes/QuizForm';
@@ -21,50 +22,111 @@ import Attendance from './attendance/Attendance';
 import Assignments from './assignments/Assignments';
 import CreateAssignment from './assignments/CreateAssignment';
 
-
-
 class Course extends React.Component {
+  state = { sidebar: false };
+
   componentDidMount() {
     const { match: { params: { id }}, dispatch } = this.props;
     dispatch(setActiveCourse(id))
   }
 
-  // componentWillUnmount() {
-  //   this.props.dispatch(clearActiveCourse())
-  // }
+  toggleSidebar = () => { this.setState({ sidebar: !this.state.sidebar }) };
+
+  sidebarItems = () => {
+    const { match: { params: { id: courseId } } } = this.props;
+    const navs = [
+      { name: 'Home', path: `/courses/${courseId}` },
+      { name: 'People', path: `/courses/${courseId}/people` },
+      { name: 'Attendance', path: `/courses/${courseId}/attendance` },
+      { name: 'Sections', path: `/courses/${courseId}/sections` },
+      { name: 'Quizzes', path: `/courses/${courseId}/quizzes` },
+      { name: 'Lectures', path: `/courses/${courseId}/lectures` },
+      { name: 'Assignments', path: `/courses/${courseId}/assignments` },
+      { name: 'Wiki', path: `/courses/${courseId}/wiki` }
+    ]
+    return navs.map(nav => {
+      return (
+        <Link to={nav.path}>
+          <Menu.Item
+            key={nav.name}
+            position='right'
+            name={nav.name}
+            onClick={() => {
+              if (this.state.sidebar)
+                this.setState({ sidebar: false });
+            }}
+          />
+        </Link>
+      )
+    })
+  }
+
+  renderRoutes = () => (
+    <Segment basic>
+      <Switch>
+        <Route exact path="/courses/:id" component={CourseHome} />
+        <Route path="/courses/:id/people" component={People} />
+        <Route path="/courses/:id/sections" component={SectionSelect} />
+        <Route path="/courses/:id/settings" component={CourseSettings} />
+        <Route path="/courses/:id/user/:id" component={PeopleProfile} />
+        <Route path="/courses/:id/attendance" component={Attendance} />
+        <Route exact path='/courses/:id/quizzes' component={Quizzes} />
+        <Route exact path='/courses/:id/quizform' component={QuizForm} />
+        <Route exact path='/courses/:id/quizzes/:id' component={SingleQuiz} />
+        <Route exact path='/courses/:id/lectures' component={Lectures} />
+        <Route exact path='/courses/:id/assignment' component={Assignment} />
+        <Route exact path='/courses/:id/assignments' component={Assignments} />
+        <Route exact path='/courses/:id/assignments/create' component={CreateAssignment} />
+        <Route exact path='/courses/:id/wiki' component={Wiki} />
+      </Switch>
+    </Segment>
+  )
 
   render() {
+    const { sidebar } = this.state;
+
     return (
       <Segment basic>
         <Container fluid style={styles.noBorder}>
           <Grid style={styles.noBorder}>
-            <Grid.Column computer={3}>
-              <Segment style={styles.sideBar}>
-                <Route component={CourseSideNav} />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column computer={13}>
-              <Segment>
-                <Switch>
-                  <Route exact path="/courses/:id" component={CourseHome} />
-                  <Route path="/courses/:id/people" component={People} />
-                  <Route path="/courses/:id/sections" component={SectionSelect} />
-                  <Route path="/courses/:id/settings" component={CourseSettings} />
-                  <Route path="/courses/:id/user/:id" component={PeopleProfile} />
-                  <Route path="/courses/:id/attendance" component={Attendance} />
-                  <Route exact path='/courses/:id/quizzes' component={Quizzes} />
-                  <Route exact path='/courses/:id/quizform' component={QuizForm} />
-                  <Route exact path='/courses/:id/quizzes/:id' component={SingleQuiz} />
-                  <Route exact path='/courses/:id/lectures' component={Lectures} />
-                  <Route exact path='/courses/:id/lectures/create' component={CreateLecture} />                  
-                  <Route exact path='/courses/:id/lectures/:id' component={Lecture} />
-                  <Route exact path='/courses/:id/assignment' component={Assignment} />
-                  <Route exact path='/courses/:id/assignments' component={Assignments} />
-                  <Route exact path='/courses/:id/assignments/create' component={CreateAssignment} />
-                  <Route exact path='/courses/:id/wiki' component={Wiki} />
-                </Switch>
-              </Segment>
-            </Grid.Column>
+            <Grid.Row only='computer'>
+              <Grid.Column mobile={16} tablet={16} computer={2} only='computer'>
+                <Segment style={styles.sidebar} basic>
+                  <Route component={CourseSideNav} />
+                </Segment>
+              </Grid.Column>
+              <Grid.Column mobile={16} tablet={16} computer={14}>
+                { this.renderRoutes() }
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row only='mobile tablet'>
+              <Grid.Column mobile={16} tablet={16} computer={2} only='computer'>
+                <Icon
+                  name='sidebar'
+                  size='large'
+                  inverted
+                  color='black'
+                  onClick={this.toggleSidebar}
+                />
+              </Grid.Column>
+              <Grid.Column mobile={16} tablet={16} computer={14}>
+                <Sidebar.Pushable style={styles.pusher}>
+                  <Sidebar 
+                    as={Menu}
+                    animation='push'
+                    width='thin'
+                    visible={sidebar}
+                    icon='labeled'
+                    vertical
+                  >
+                      { this.sidebarItems() }
+                  </Sidebar>
+                  <Sidebar.Pusher>
+                    { this.renderRoutes() }
+                  </Sidebar.Pusher>
+                </Sidebar.Pushable>
+              </Grid.Column>
+            </Grid.Row>
           </Grid>
         </Container>
       </Segment>
@@ -76,10 +138,12 @@ const styles = {
   noBorder: {
     border:'none',
   },
-
-  sideBar: {
+  sidebar: {
     height: "65vh",
     width: '11em',
+  },
+  pusher: {
+    height: '100vh'
   }
 }
 
