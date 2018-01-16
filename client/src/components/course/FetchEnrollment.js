2d358cfbd268e025665599902281fe2df7abd6bc
cgregs32 <cgregs32@gmail.com>
2018-01-15 17:23:25 -0700

    bug fix - page reload
    Reviewers: #portal, fuchai_mead
    
    Reviewed By: #portal, fuchai_mead
    
    Differential Revision: http://phabricator.devpointstudios.com/D954

--- client/src/components/course/FetchEnrollment.js
+++ client/src/components/course/FetchEnrollment.js
@@ -5,23 +5,14 @@ import { withRouter } from 'react-router-dom';
 
 
 class FetchEnrollment extends React.Component {
-  state = { loaded: false }
 
   componentDidMount(){
     const { dispatch, match: { params: { id } } } = this.props
     dispatch(getPermissions(id))
   }
 
-  componentWillReceiveProps() {
-    if (!this.state.loaded) this.loaded();
-  }
-
-  loaded = () => {
-    this.setState({ loaded: true });
-  }
-
   render() {
-    return this.state.loaded && this.props.children
+    return this.props.children
   }
 }
 
