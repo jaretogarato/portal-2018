4aa53d0fe23ba3db889add596e495a9cc108c1c2
Emmy Avalos <emmyavalos415@gmail.com>
2018-01-19 15:28:26 -0700

    background image to login page login form changes refactor grid
    Reviewers: #portal, josh.murphy
    
    Reviewed By: #portal, josh.murphy
    
    Differential Revision: http://phabricator.devpointstudios.com/D1014

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import HomeBg from '../assets/images/home-image-2880w.jpg';
 import { connect } from 'react-redux';
 import { handleLogin } from '../actions/auth';
-import { Button, Form, Header, Grid, Segment } from 'semantic-ui-react';
+import { Button, Container, Form, Header, Grid, Segment } from 'semantic-ui-react';
+import { HomeBody, HomeLogo, HomeWrapper } from '../styles/home-images.js';
+
+import PortalLogo from '../assets/images/dps-portal-logo.png';
+
 
 class Login extends Component {
   state = { email: '', password: '' };
@@ -21,44 +26,57 @@ class Login extends Component {
   render() {
     const { email, password } = this.state;
     return (
-      <div>
-        <Grid textAlign='center'>
-          <Grid.Column style={{ maxWidth: 450, padding: '5%' }}>
-            <Segment>
-              <Header>Login</Header>
-              <Form onSubmit={this.handleSubmit}>
-                <Form.Field>
-                  <label htmlFor='email'>Email</label>
-                  <input
-                  icon='user'
-                  iconposition='left'                  
-                  required
-                  id='email'
-                  value={email}
-                  placeholder='Email'
-                  onChange={this.handleChange}
-                />
-              </Form.Field>
-              <Form.Field>
-                <label htmlFor='password'>Password</label>
-                <input
-                  icon='lock'
-                  iconposition='left'
-                  required
-                  id='password'
-                  value={password}
-                  placeholder='Password'
-                  type='password'
-                  onChange={this.handleChange}
-                />
-              </Form.Field>
-              <Button color='blue'>Login</Button>
-              </Form>
+      <Container fluid>
+        <HomeBody bgImage={HomeBg}>
+          <HomeWrapper>
+            <Segment basic textAlign='center'>
+              <Grid textAlign='center' style={{ maxWidth: 450, padding: '5%', margin: '0 auto'}}>
+                <Grid.Column style={styles.login_container}>
+                  <Header as="h2">Login</Header>
+                  <Form onSubmit={this.handleSubmit}>
+                    <Form.Field>
+                      <label htmlFor='email'>Email</label>
+                      <input
+                        icon='user'
+                        iconposition='left'
+                        required
+                        id='email'
+                        value={email}
+                        placeholder='Email'
+                        onChange={this.handleChange}
+                      />
+                    </Form.Field>
+                    <Form.Field>
+                      <label htmlFor='password'>Password</label>
+                      <input
+                        icon='lock'
+                        iconposition='left'
+                        required
+                        id='password'
+                        value={password}
+                        placeholder='Password'
+                        type='password'
+                        onChange={this.handleChange}
+                      />
+                    </Form.Field>
+                    <Button color='blue'>Login</Button>
+                  </Form>
+                </Grid.Column>
+              </Grid>
             </Segment>
-          </Grid.Column>
-        </Grid>
-      </div>
+          </HomeWrapper>
+        </HomeBody>
+      </Container>
     )
   }
 }
+
+const styles = {
+  login_container: {
+    backgroundColor: 'rgba(255, 255, 255, .55)',
+    padding: '30px 20px',
+    maxWidth: '450px',
+  },
+}
+
 export default connect()(Login);
