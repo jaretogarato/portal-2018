a795bfb10440ca1ce97d25ba39ccdb9415f455ff
Isabel Juliao-Martin <isabel.youthboard@futureindesign.com>
2018-01-25 00:35:15 -0700

    course homepage responsive
    Reviewers: #portal, bryan-leano
    
    Reviewed By: #portal, bryan-leano
    
    Differential Revision: http://phabricator.devpointstudios.com/D1121

--- client/src/components/course/CourseHome.js
+++ client/src/components/course/CourseHome.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import {
-  Container,
-  Image,
-} from 'semantic-ui-react';
+import { Container, Header, Segment, Grid, Image, List } from 'semantic-ui-react';
+import devpoint from '../../assets/images/devpoint.png'
+import { HomeStrings } from '../../assets/strings/homeStrings'
+import { PageTitle, PageSubTitle, BoldText, ParagraphText } from '../../styles/styledComponents'
+import CourseHomeDropDown from './CourseHomeDropdown'
 
-import devpoint from '../../assets/images/devpoint.png';
-import { HomeStrings } from '../../assets/strings/homeStrings';
-import {
-  PageTitle,
-  PageSubTitle,
-  ParagraphText,
-} from '../../styles/styledComponents';
-
-const first = HomeStrings.concepts.slice(0, 4)
-const second = HomeStrings.concepts.slice(4,8)
-const third = HomeStrings.concepts.slice(8,12)
-const fourth = HomeStrings.concepts.slice(12,16)
-const fifth = HomeStrings.concepts.slice(16,20)
-const sixth = HomeStrings.concepts.slice(20,24)
-const seventh = HomeStrings.concepts.slice(24,28)
-const eighth = HomeStrings.concepts.slice(28,32)
-
+//merge conflicts
 class CourseHome extends React.Component {
+  // see comments below for usage of state
+  // it starts with truly
+  // state = { displaySwitch: false }
 
   displayConcepts = (input) => {
     return input.map( concept => {
@@ -33,149 +21,183 @@ class CourseHome extends React.Component {
 
   displayWhatToLearn = (input) => {
     return input.map ( input => {
-      return <li>{ input }</li>
+      return <li>{input}</li>
     })
   }
 
+  // if wanting the original mock table from canvas, see this
+  // conceptsTable = () => {
+  //  const first = HomeStrings.concepts.slice(0, 4)
+  //  const second = HomeStrings.concepts.slice(4,8)
+  //  const third = HomeStrings.concepts.slice(8,12)
+  //  const fourth = HomeStrings.concepts.slice(12,16)
+  //  const fifth = HomeStrings.concepts.slice(16,20)
+  //  const sixth = HomeStrings.concepts.slice(20,24)
+  //  const seventh = HomeStrings.concepts.slice(24,28)
+  //  const eighth = HomeStrings.concepts.slice(28,32)
+  //   return (
+  //     <table style={{width: '100%'}}>
+  //       <tbody>
+  //         <tr>
+  //           {this.displayConcepts(first)}
+  //         </tr>
+  //         <tr>
+  //           {this.displayConcepts(second)}
+  //         </tr>
+  //         <tr>
+  //           {this.displayConcepts(third)}
+  //         </tr>
+  //         <tr>
+  //           {this.displayConcepts(fourth)}
+  //         </tr>
+  //         <tr>
+  //           {this.displayConcepts(fifth)}
+  //         </tr>
+  //         <tr>
+  //           {this.displayConcepts(sixth)}
+  //         </tr>
+  //         <tr>
+  //           {this.displayConcepts(seventh)}
+  //         </tr>
+  //         <tr>
+  //           {this.displayConcepts(eighth)}
+  //         </tr>
+  //       </tbody>
+  //     </table>
+  //   )
+  // }
+
+  // this is the switch for displaying table or dropdown with concepts
+  // checkSize = () => {
+  //   if (window.innerWidth > 768){
+  //     return this.setState({ displaySwitch: true })
+  //   } else {
+  //     return this.setState({ displaySwitch: false})
+  //   }
+  // }
+  
+  
+
   render() {
+    // if wanted responsive switch use function below
+    // window.onresize = this.checkSize
     return(
       <Container>
-        <div style={{ height: '250px', display: 'flex' }}>
-          <Image src={ devpoint } alt='DevPoint Labs' style={{ margin: 'auto' }} />
-        </div>
-        <h3>Main Website</h3>
-        <h5>
-          <a href='http://devpointlabs.com' style={{ color: 'purple' }}>Main Site</a>
-        </h5>
-        <h3>Student Handbook</h3>
-        <h5>
-          <a href='https://docs.google.com/document/d/1ly9CTnGcYb6Rls-92RpKCR9RHJ1IcvdBSmfy7HZj3NM/edit' style={{ color: 'purple' }}>Student Handbook</a>
-        </h5>
-        <h3>Concepts</h3>
-        <table style={{ width: '100%' }}>
-          <tbody>
-            <tr>
-             { this.displayConcepts(first) }
-            </tr>
-            <tr>
-             { this.displayConcepts(second) }
-            </tr>
-            <tr>
-             { this.displayConcepts(third) }
-            </tr>
-            <tr>
-             { this.displayConcepts(fourth) }
-            </tr>
-            <tr>
-             { this.displayConcepts(fifth) }
-            </tr>
-            <tr>
-             { this.displayConcepts(sixth) }
-            </tr>
-            <tr>
-             { this.displayConcepts(seventh) }
-            </tr>
-            <tr>
-             { this.displayConcepts(eighth) }
-            </tr>
-          </tbody>
-        </table>
-        <PageTitle>
-          { HomeStrings.whatyouWillLearnHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.whatyouWillLearnH2 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.whatyouWillLearnH3 }
-        </ParagraphText>
-        <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc" }}>
-          {  this.displayWhatToLearn(HomeStrings.whatyouWillLearnList) }
-        </ul>
-        <PageTitle>
-          { HomeStrings.usingCanvasHeader }
-        </PageTitle>
-        <PageSubTitle>
-          { HomeStrings.usingCanvasModule }
-        </PageSubTitle>
-        <ParagraphText>
-          { HomeStrings.usingCanvasModuleText1 }
-        </ParagraphText>
-        <ParagraphText>
-         { HomeStrings.usingCanvasModuleText2 }
-        </ParagraphText>
-        <ParagraphText>
-         { HomeStrings.usingCanvasModuleText3 }
-        </ParagraphText>
-        <ParagraphText>
-         { HomeStrings.usingCanvasModuleText4 }
-        </ParagraphText>
-        <PageTitle>
-         { HomeStrings.usingCanvasAnnouncementsHeader }
-        </PageTitle>
-        <ParagraphText>
-         { HomeStrings.usingCanvasAnnouncementsText }
-        </ParagraphText>
-        <PageTitle>
-         { HomeStrings.primaryPortfolioProjectHeader }
-        </PageTitle>
-        <ParagraphText>
-         { HomeStrings.primaryPortfolioProjectIntroText1 }
-        </ParagraphText>
-        <ParagraphText>
-         { HomeStrings.primaryPortfolioProjectIntroText2 }
-        </ParagraphText>
-        <PageTitle>
-         { HomeStrings.primaryPortfolioProjectInHouseHeader }
-        </PageTitle>
-        <ParagraphText>
-         { HomeStrings.primaryPortfolioProjectInHouseText1 }
-        </ParagraphText>
-        <ParagraphText>
-         { HomeStrings.primaryPortfolioProjectInHouseText2 }
-        </ParagraphText>
-        <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc" }}>
-        { this.displayWhatToLearn(HomeStrings.primaryPortfolioProjectInHouseList1) }
-        </ul>
-        <PageTitle>
-          { HomeStrings.primaryPortfolioProjectCustomerHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.primaryPortfolioProjectCustomerText1 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.primaryPortfolioProjectCustomerText2 }
-        </ParagraphText>
-        <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc" }}>
-          { this.displayWhatToLearn(HomeStrings.primaryPortfolioProjectCustomerList) }
-        </ul>
-        <PageTitle>
-          {HomeStrings.gradingHeader}
-        </PageTitle>
-        <ParagraphText>
-          {HomeStrings.gradingText}
-        </ParagraphText>
-        <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc" }}>
-          { this.displayWhatToLearn(HomeStrings.gradingList) }
-        </ul>
-        <PageTitle>
-         { HomeStrings.attendanceHeader }
-        </PageTitle>
-        <ParagraphText>
-         { HomeStrings.attendanceText }
-        </ParagraphText>
-        <PageTitle>
-         { HomeStrings.assignmentsHeader }
-        </PageTitle>
-        <ParagraphText>
-         { HomeStrings.assignmentsText }
-        </ParagraphText>
-        <PageTitle>
-         { HomeStrings.careerServicesHeader }
-        </PageTitle>
-        <ParagraphText>
-         { HomeStrings.careerServicesText }
-        </ParagraphText>
+              <div style={{height: '250px', display: 'flex'}}>
+                <Image src={devpoint} alt='devpoint' style={{margin: 'auto', width: '90%'}} />
+              </div>
+              <h3>
+                Main Website
+              </h3>
+              <h5>
+                <a href='http://devpointlabs.com' style={{color: 'purple'}}> Main Site </a>
+              </h5>
+              <h3>
+                Student Handbook
+              </h3>
+              <h5>
+                <a href='https://docs.google.com/document/d/1ly9CTnGcYb6Rls-92RpKCR9RHJ1IcvdBSmfy7HZj3NM/edit' style={{color: 'purple'}}> Student Handbook </a>
+              </h5>
+              <h3>
+                Concepts
+              </h3>
+              <CourseHomeDropDown stateOptions={HomeStrings.concepts} />
+              <PageTitle>
+                {HomeStrings.whatyouWillLearnHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.whatyouWillLearnH2}
+              </ParagraphText>
+              <ParagraphText>
+                {HomeStrings.whatyouWillLearnH3}
+              </ParagraphText>
+              <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc"}}>
+                { this.displayWhatToLearn(HomeStrings.whatyouWillLearnList) }
+              </ul>
+              <PageTitle>
+                {HomeStrings.usingCanvasHeader}
+              </PageTitle>
+              <PageSubTitle>
+                {HomeStrings.usingCanvasModule}
+              </PageSubTitle>
+              <ParagraphText>
+                {HomeStrings.usingCanvasModuleText1}
+              </ParagraphText>
+              <ParagraphText>
+                {HomeStrings.usingCanvasModuleText2}
+              </ParagraphText>
+              <ParagraphText>
+                {HomeStrings.usingCanvasModuleText3}
+              </ParagraphText>
+              <ParagraphText>
+                {HomeStrings.usingCanvasModuleText4}
+              </ParagraphText>
+              <PageTitle>
+                {HomeStrings.usingCanvasAnnouncementsHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.usingCanvasAnnouncementsText}
+              </ParagraphText>
+              <PageTitle>
+                {HomeStrings.primaryPortfolioProjectHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.primaryPortfolioProjectIntroText1}
+              </ParagraphText>
+              <ParagraphText>
+                {HomeStrings.primaryPortfolioProjectIntroText2}
+              </ParagraphText>
+              <PageTitle>
+                {HomeStrings.primaryPortfolioProjectInHouseHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.primaryPortfolioProjectInHouseText1}
+              </ParagraphText>
+              <ParagraphText>
+                {HomeStrings.primaryPortfolioProjectInHouseText2}
+              </ParagraphText>
+              <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc"}}>
+                { this.displayWhatToLearn(HomeStrings.primaryPortfolioProjectInHouseList1) }
+              </ul>
+              <PageTitle>
+                {HomeStrings.primaryPortfolioProjectCustomerHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.primaryPortfolioProjectCustomerText1}
+              </ParagraphText>
+              <ParagraphText>
+                {HomeStrings.primaryPortfolioProjectCustomerText2}
+              </ParagraphText>
+              <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc"}}>
+                { this.displayWhatToLearn(HomeStrings.primaryPortfolioProjectCustomerList) }
+              </ul>
+              <PageTitle>
+                {HomeStrings.gradingHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.gradingText}
+              </ParagraphText>
+              <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc"}}>
+                { this.displayWhatToLearn(HomeStrings.gradingList) }
+              </ul>
+              <PageTitle>
+                {HomeStrings.attendanceHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.attendanceText}
+              </ParagraphText>
+              <PageTitle>
+                {HomeStrings.assignmentsHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.assignmentsText}
+              </ParagraphText>
+              <PageTitle>
+                {HomeStrings.careerServicesHeader}
+              </PageTitle>
+              <ParagraphText>
+                {HomeStrings.careerServicesText}
+              </ParagraphText>
       </Container>
     );
   }
---
+++ client/src/components/course/CourseHomeDropdown.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Dropdown } from 'semantic-ui-react'
+import { HomeStrings } from '../../assets/strings/homeStrings'
+
+let coolArray = [{text:"HTML"}, {text:"CSS"}, {text: 'a11y'}, {text:'JavaScript'}, {text: 'Ruby'}, {text: 'Command Line'}, {text: 'Functional Programming'}, {text: 'Real Time Applications'}, {text: 'UI Frameworks'}, {text: 'Ruby'}, {text: 'GIT'}, {text: 'SQL'}, {text: 'jQuery'}, {text: 'OOP'}, {text: 'PWA\'s'}, {text: "Rails"}, {text: "Authentication"}, {text: "Deployment"}, {text: "Testing",}, {text: "QA"}, {text: "AJAX / Fetch / Axios"}, {text: "Performance"}, {text: "Package Publishing"}, {text: "ESNext"}, {text: "ReactJS"}, {text: "Flux"}, {text: "Redux"}, {text: "Webpack"}, {text: "Routing"}, {text: "Native"}, {text: "More..."}]
+
+const CourseHomeDropDown = () => (
+  <Dropdown placeholder='Explore DevPoint Concepts' fluid search selection options={coolArray} />
+)
+
+export default CourseHomeDropDown
