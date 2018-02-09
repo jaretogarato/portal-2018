c6133d99fb93fb3ef7aae3ea8041f118f513ccf6
Dan Actor <danltactor@gmail.com>
2018-02-09 09:52:47 -0700

    home page
    Reviewers: #portal, #staff, mbpeterson19, amymstevens
    
    Reviewed By: #portal, #staff, mbpeterson19
    
    Differential Revision: http://phabricator.devpointstudios.com/D1088

--- client/src/components/course/CourseHome.js
+++ client/src/components/course/CourseHome.js
@@ -9,7 +9,6 @@ import {
 } from '../../styles/styledComponents';
 import CourseHomeDropDown from './CourseHomeDropdown';
 
-
 class CourseHome extends React.Component {
 
   displayConcepts = (input) => {
@@ -25,123 +24,203 @@ class CourseHome extends React.Component {
       return <li>{input}</li>
     })
   }
-  
 
   render() {
     return(
       <Container>
-        <div style={{height: '250px', display: 'flex'}}>
-          <Image src={devpoint} alt='devpoint' style={{margin: 'auto', width: '90%'}} />
-        </div>
-        <h3>Main Website</h3>
-        <h5>
-          <a href='http://devpointlabs.com' style={{color: 'purple'}}> Main Site </a>
-        </h5>
-        <h3>Student Handbook</h3>
-        <h5>
-          <a href='https://docs.google.com/document/d/1ly9CTnGcYb6Rls-92RpKCR9RHJ1IcvdBSmfy7HZj3NM/edit' style={{color: 'purple'}}> Student Handbook </a>
-        </h5>
-        <h3>Concepts</h3>
-        <CourseHomeDropDown stateOptions={HomeStrings.concepts} />
-        <PageTitle>
-          { HomeStrings.whatyouWillLearnHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.whatyouWillLearnH2 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.whatyouWillLearnH3 }
-        </ParagraphText>
-        <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc"}}>
-          { this.displayWhatToLearn(HomeStrings.whatyouWillLearnList) }
-        </ul>
-        <PageTitle>
-          { HomeStrings.usingCanvasHeader }
-        </PageTitle>
-        <PageSubTitle>
-          { HomeStrings.usingCanvasModule }
-        </PageSubTitle>
-        <ParagraphText>
-          { HomeStrings.usingCanvasModuleText1 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.usingCanvasModuleText2 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.usingCanvasModuleText3 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.usingCanvasModuleText4 }
-        </ParagraphText>
-        <PageTitle>
-          { HomeStrings.usingCanvasAnnouncementsHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.usingCanvasAnnouncementsText }
-        </ParagraphText>
-        <PageTitle>
-          { HomeStrings.primaryPortfolioProjectHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.primaryPortfolioProjectIntroText1 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.primaryPortfolioProjectIntroText2 }
-        </ParagraphText>
-        <PageTitle>
-          { HomeStrings.primaryPortfolioProjectInHouseHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.primaryPortfolioProjectInHouseText1 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.primaryPortfolioProjectInHouseText2 }
-        </ParagraphText>
-        <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc"}}>
-        { this.displayWhatToLearn(HomeStrings.primaryPortfolioProjectInHouseList1) }
-        </ul>
-        <PageTitle>
-          { HomeStrings.primaryPortfolioProjectCustomerHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.primaryPortfolioProjectCustomerText1 }
-        </ParagraphText>
-        <ParagraphText>
-          { HomeStrings.primaryPortfolioProjectCustomerText2 }
-        </ParagraphText>
-        <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc"}}>
-          {  this.displayWhatToLearn(HomeStrings.primaryPortfolioProjectCustomerList) }
-        </ul>
-        <PageTitle>
-          { HomeStrings.gradingHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.gradingText }
-        </ParagraphText>
-        <ul style={{ display: 'flex', flexDirection: 'column', listStyleType: "disc"}}>
-          {  this.displayWhatToLearn(HomeStrings.gradingList) }
-        </ul>
-        <PageTitle>
-          { HomeStrings.attendanceHeader }
-        </PageTitle>
-        <ParagraphText>
-          { HomeStrings.attendanceText }
-        </ParagraphText>
-        <PageTitle>
-          { HomeStrings.assignmentsHeader }
-        </PageTitle>
-        <ParagraphText>
-          {HomeStrings.assignmentsText }
-        </ParagraphText>
-        <PageTitle>
-          {HomeStrings.careerServicesHeader }
-        </PageTitle>
-        <ParagraphText>
-          {HomeStrings.careerServicesText }
-        </ParagraphText>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={1} stackable>
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <div>
+                <Image src={devpoint} alt='devpoint' />
+              </div>
+            </Grid.Column>
+            <Grid.Column width={1} stackable>
+              <div >
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1} stackable>
+            </Grid.Column>
+            <Grid.Column width={14} stackable>
+              <h3>
+                Main Website
+              </h3>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1} stackable>
+            </Grid.Column>
+            <Grid.Column width={14} stackable>
+              <h5>
+                <a href='http://devpointlabs.com' style={{color: 'purple'}}> Main Site </a>
+              </h5>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1}  stackable >
+            </Grid.Column>
+            <Grid.Column width={14} stackable >
+              <h3>
+                Student Handbook
+              </h3>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1} stackable >
+            </Grid.Column>
+            <Grid.Column width={14} stackable >
+              <h5>
+                <a href='https://docs.google.com/document/d/1ly9CTnGcYb6Rls-92RpKCR9RHJ1IcvdBSmfy7HZj3NM/edit' style={{color: 'purple'}}> Student Handbook </a>
+              </h5>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1} stackable>
+            </Grid.Column>
+            <Grid.Column width={14} stackable>
+              <h3>
+                Concepts
+                </h3>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1} stackable>
+            </Grid.Column>
+            <Grid.Column width={4} stackable>
+              <p style={styles.paragraph1}>skdjaljldjaskld
+                lksjdlasjdlasjd
+                slkjdlkasjdkl
+                asldjkaljd
+              </p>
+            </Grid.Column>
+            <Grid.Column width={4} stackable>
+              <p style={styles.paragraph1}>skdjaljldjaskld
+                lksjdlasjdlasjd
+                slkjdlkasjdkl
+                asldjkaljd
+              </p>
+            </Grid.Column>
+            <Grid.Column width={4} stackable>
+              <p style={styles.paragraph1}>skdjaljldjaskld
+                lksjdlasjdlasjd
+                slkjdlkasjdkl
+                asldjkaljd
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1} stackable>
+            </Grid.Column>
+            <Grid.Column width={14} stackable>
+              <h3>
+              What you will learn
+              </h3>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1} stackable>
+            </Grid.Column>
+            <Grid.Column width={4} stackable >
+              <p style={styles.paragraph2}>skdjaljldjaskld
+                lksjdlasjdlasjd
+                slkjdlkasjdkl
+                asldjkaljdsjdhasdkajshdk
+                sdhhdahsakjhdjkashdjkhaskj
+                shdsajdlkasjdkljldkjslajdklasjdklasjkldja
+                slkjdslajdsjdkladkl
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1}>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <h3>
+                Grading
+              </h3>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1}>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <p style={styles.paragraph2}>skdjaljldjaskld
+                lksjdlasjdlasjd
+                slkjdlkasjdkl
+                asldjkaljdsjdhasdkajshdk
+                sdhhdahsakjhdjkashdjkhaskj
+                shdsajdlkasjdkljldkjslajdklasjdklasjkldja
+                slkjdslajdsjdkladkl
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1}>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <h3>
+                Attendence
+              </h3>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1}>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <p style={styles.paragraph2}>skdjaljldjaskld
+                lksjdlasjdlasjd
+                slkjdlkasjdkl
+                asldjkaljdsjdhasdkajshdk
+                sdhhdahsakjhdjkashdjkhaskj
+                shdsajdlkasjdkljldkjslajdklasjdklasjkldja
+                slkjdslajdsjdkladkl
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1}>
+            </Grid.Column>
+            <Grid.Column width={14}>
+              <h3>
+                Assingments
+              </h3>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={1}>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <p style={styles.paragraph2}>skdjaljldjaskld
+                lksjdlasjdlasjd
+                slkjdlkasjdkl
+                asldjkaljdsjdhasdkajshdk
+                sdhhdahsakjhdjkashdjkhaskj
+                shdsajdlkasjdkljldkjslajdklasjdklasjkldja
+                slkjdslajdsjdkladkl
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     );
   }
+}
+
+const styles = {
+  paragraph1: {
+    textAlign: 'center',
+    fontSize: '1em',
+    color: 'black',
+  },
+  paragraph2: {
+    textAlign: 'left',
+    fontSize: '1em',
+    color: 'black',
+  },
 }
 
 export default CourseHome;
