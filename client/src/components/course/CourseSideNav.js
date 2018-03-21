7a15c5785909ca349d0d13ece6f074dd1a2d2298
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-03-21 16:14:58 -0600

    Reorder Sidebar items
    Reviewers: #portal, djungst
    
    Reviewed By: #portal, djungst
    
    Differential Revision: http://phabricator.devpointstudios.com/D1341

--- client/src/components/course/CourseSideNav.js
+++ client/src/components/course/CourseSideNav.js
@@ -14,10 +14,17 @@ class CourseSideNav extends React.Component {
     const { user, match: { params: { id }}} = this.props;
     const { activeItem } = this.state;
     const adminLinks = [
-      'Home', 'Announcements', 'People', 'Attendance', 'Sections',
-      'Quizzes', 'Lectures', 'Assignments', 'Wiki', 'Settings'
+      'Home',
+      'Announcements',
+      'Assignments',
+      'Attendance',
+      'Lectures',
+      'People',
+      'Quizzes',
+      'Sections',
+      'Wiki',
+      'Settings',
     ];
-
     const studentLinks = [
       'Home', 'Announcements', 'People', 'Sections', 'Wiki'
     ];
@@ -46,8 +53,12 @@ class CourseSideNav extends React.Component {
         to={`/courses/${id}${link === 'Home' ? '' : `/${link.toLowerCase()}`}`}
       >
         <Menu fluid vertical style={styles.sideNav}>
+<<<<<<< HEAD
           <Menu.Item 
             as={ParagraphText}
+=======
+          <Menu.Item
+>>>>>>> changes order of menu on sidebar
             name={link}
             active={activeItem === `${link}`}
             onClick={this.handleItemClick}
